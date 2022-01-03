c366c712bc7b8fbba579c7af2783f6ecf65c7674
Mark Rofaeel <mark.rofaeel@gmail.com>
2022-01-03 02:34:43 +0200

    Update api.service.ts

--- Frontend/src/app/api.service.ts
+++ Frontend/src/app/api.service.ts
@@ -8,27 +8,27 @@ export class ApiService {
   constructor(private http:HttpClient) { }
   registerUser(user: any)
   {
-    this.http.post('http://localhost:3000/api/user',user).subscribe(res=>{
+    this.http.post('http://backend-todolistapp.apps.eu45.prod.nextcle.com/api/user',user).subscribe(res=>{
       console.log(res);
     })
   }
   loginUser(user: any){
-    this.http.post('http://localhost:3000/api/userlogin',user).subscribe(res=>{
+    this.http.post('http://backend-todolistapp.apps.eu45.prod.nextcle.com/api/userlogin',user).subscribe(res=>{
       console.log(res);
     })
   }
   public addtask(task: any){
-    this.http.post('http://localhost:3000/api/task',{"title":task,"isDone":false}).subscribe(res=>{
+    this.http.post('http://backend-todolistapp.apps.eu45.prod.nextcle.com/api/task',{"title":task,"isDone":false}).subscribe(res=>{
       console.log(res);
     })
 }
   public removetask(id: string){
-  this.http.delete('http://localhost:3000/api/task/'+id).subscribe(res=>{
+  this.http.delete('http://backend-todolistapp.apps.eu45.prod.nextcle.com/api/task/'+id).subscribe(res=>{
     console.log(res);
   })
 }
   public gettasks(){
-    this.http.get('http://localhost:3000/api/tasks').subscribe(res=>{
+    this.http.get('http://backend-todolistapp.apps.eu45.prod.nextcle.com/api/tasks').subscribe(res=>{
       console.log(res);
     })
 }
